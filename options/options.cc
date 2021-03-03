7a03b3ff11986eafc4c9128514965dd6a0626b26
jt2594838 <jt2594838@163.com>
2021-03-03 03:10:19 -0800

    change low level compression

--- options/options.cc
+++ options/options.cc
@@ -554,14 +554,10 @@ ColumnFamilyOptions* ColumnFamilyOptions::OptimizeLevelStyleCompaction(
   // only compress levels >= 2
   compression_per_level.resize(num_levels);
   for (int i = 0; i < num_levels; ++i) {
-    if (i < 2) {
-      compression_per_level[i] = kNoCompression;
-    } else {
-      compression_per_level[i] =
-          LZ4_Supported()
-              ? kLZ4Compression
-              : (Snappy_Supported() ? kSnappyCompression : kNoCompression);
-    }
+    compression_per_level[i] =
+        LZ4_Supported()
+        ? kLZ4Compression
+        : (Snappy_Supported() ? kSnappyCompression : kNoCompression);
   }
   return this;
 }
