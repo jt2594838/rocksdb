7be7285ee3ff2b93e294f5daa5785a7bd7443673
jt2594838 <jt2594838@163.com>
2021-03-10 22:42:36 -0800

    add build table logs

--- db/builder.cc
+++ db/builder.cc
@@ -168,6 +168,7 @@ Status BuildTable(
         ShouldReportDetailedTime(env, ioptions.statistics),
         true /* internal key corruption is not ok */, range_del_agg.get());
     c_iter.SeekToFirst();
+    ROCKS_LOG_INFO(ioptions.info_log, "[JOB %d] start to build table", job_id);
     for (; c_iter.Valid(); c_iter.Next()) {
       const Slice& key = c_iter.key();
       const Slice& value = c_iter.value();
@@ -197,6 +198,8 @@ Status BuildTable(
       meta->UpdateBoundariesForRange(kv.first, tombstone.SerializeEndKey(),
                                      tombstone.seq_, internal_comparator);
     }
+
+    ROCKS_LOG_INFO(ioptions.info_log, "[JOB %d] new table is build", job_id);
 
     // Finish and check for builder errors
     bool empty = builder->IsEmpty();
@@ -281,6 +284,7 @@ Status BuildTable(
         }
       }
     }
+    ROCKS_LOG_INFO(ioptions.info_log, "[JOB %d] new table is checked", job_id);
   }
 
   // Check for input iterator errors
